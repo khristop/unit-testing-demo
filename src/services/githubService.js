10358f0348f074f311d78fe3d3ec901e7bc220f9
khristop <khristop@users.noreply.github.com>
2023-02-18 09:25:43 +0000

    deploy: 33f5ea809337c83a5a2939397847785ddfbfd449

--- src/services/githubService.js
+++
@@ -1,7 +0,0 @@
-export const apiURL = "https://api.github.com";
-
-export const searchUserProfiles = (hint) => {
-  return fetch(`${apiURL}/search/users?q=${hint}`)
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-};
