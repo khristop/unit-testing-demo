edf789140cfd8518ac9fb0be4c3c0e6c01093a9e
khristop <kgarciamartine00@mylangara.ca>
2023-02-17 18:28:25 -0800

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Search from './components/search/Search';
+import { searchUserProfiles } from './services/githubService';
 
 function App() {
+  const [userList, setUserList] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const searchProfiles = (hint) => {
+    setLoading(true);
+    searchUserProfiles(hint)
+      .then(data => {
+        setUserList(data.items);
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub Profile App</h1>
+      <Search onSearch={searchProfiles} loading={loading}></Search>
+      <div className="user-list">
+        {userList && userList.map(user => (
+          <div className="user" key={user.id}>
+            <img src={user.avatar_url} alt={user.login} />
+            <div className="user-info">
+              <h2>{user.login}</h2>
+              <p>{user.bio}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
